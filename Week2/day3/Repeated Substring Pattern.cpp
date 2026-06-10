d282478ab61bf9d05da264884e4fa569fe4568a1
Vikash Rao <vikashrao6663@gmail.com>
2026-06-10 22:10:18 +0530

    week2 day 3 solution

---
+++ Week2/day3/Repeated Substring Pattern.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.size();
+
+        for (int len = 1; len <= n / 2; len++) {
+
+            if (n % len != 0) continue;
+
+            string part = s.substr(0, len);
+            string temp = "";
+
+            int times = n / len;
+
+            for (int i = 0; i < times; i++) {
+                temp += part;
+            }
+
+            if (temp == s) return true;
+        }
+
+        return false;
+    }
+};
