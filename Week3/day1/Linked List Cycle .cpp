828e1e482ce8b3e29b297a1e357f7c68ff8da12d
Vikash Rao <vikashrao6663@gmail.com>
2026-06-16 00:24:25 +0530

    week3 day 1

---
+++ Week3/day1/Linked List Cycle .cpp
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;          // move 1 step
+            fast = fast.next.next;     // move 2 steps
+
+            if (slow == fast) {
+                return true;           // cycle detected
+            }
+        }
+
+        return false;                  // no cycle
+    }
+}
